103337cfa752ac71859850b8825047cbc260ea17
nievesag <nievalon@ucm.es>
2023-09-22 08:07:37 +0200

    comentando cosas

--- TPV1 labo/Ejercicio 1 hoja 1/Ejercicio 1 hoja 1.cpp
+++ TPV1 labo/Ejercicio 1 hoja 1/Ejercicio 1 hoja 1.cpp
@@ -1,24 +1,28 @@
-// 
-//
+// Inés Primo López
+// Nieves Alonso Gilsanz
 
 #include <iostream>
-#include<array>
+#include<array> // incluye la nomenclatura moderna para los arrays 
 using namespace std;
 
-void MoveVector(array<int, 5> vector, int tam)
+// método que reordena el vector
+void MoveVector(array<int, 5> vector, int tam) // se le pasa el array<tipo, tamaño> -> vector resultado / entero tam para el for
 {
-	array<int, 5> a;
-	for (int i = 0; i < tam; i++)
+	array<int, 5> a; // declaración del array a de tamaño 5 elementos -> vector inicial
+	for (int i = 0; i < tam; i++) // recorre el array hasta tam (se pasa su valor en main)
 	{
-		a[(i + 1) % a.size()] = vector[i];
+		a[(i + 1) % a.size()] = vector[i]; // áplicación de la fórmula del array circular para reordenar a en vector
+										   // la posición 0 en "a" va a ser la 1 en en "vector" etc...
 	}
 
-	for (int i = 0; i < vector.size(); i++)
+	for (int i = 0; i < vector.size(); i++) // recorre vector entero
 	{
-		cout << a[i];
+		cout << a[i]; // escribe a reorganizado
 	}
 }
 
+// clase vector xd
+/* 
 #pragma once
 class Vector
 {
@@ -38,23 +42,24 @@ public:
 		return a;
 	}
 
-};
+}; */
 
-int main()
+int main() 
 {
-	const int tam = 5;
-	array<int, 5> vector = { 0, 1, 2, 3, 4 }; // vector uwuwuwuwu
+	const int tam = 5; // establece el tamaño del array a reordenar
+	array<int, 5> vector = { 0, 1, 2, 3, 4 }; // inicializa el vector
 
-	for (int i = 0; i < vector.size(); i++)
+	for (int i = 0; i < vector.size(); i++) // recorre vector
 	{
-		cout << vector[i];
+		cout << vector[i]; // va escribiendo el vector antes de ser reorganizado 
+						   // (lo que hemos inicializado antes)
 	}
 
-	cout << "\n";
+	cout << "\n"; // salto de línea
 	
 	std::cout << "Hello World!\n"; // gonorrea
 
-	MoveVector(vector, tam); // puntero para guardar la direccion
+	MoveVector(vector, tam); // ejecuta el método
 
 	return 666;
 }
