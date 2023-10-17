5b6e4a45d3674042273e146e3591d09295b8fb70
anonymous <nievalon@ucm.es>
2023-10-17 16:04:14 +0200

    no se agrega bien D:

---
+++ TPV1 labo/Invasores del espacio/SpaceInvaders/spaceInvadersSDL/main.cpp
@@ -0,0 +1,18 @@
+#include "checkML.h"
+
+#include "SDL.h"
+#include "SDL_image.h"
+#include <iostream>
+#include <Windows.h>
+#include <vector>
+
+using namespace std;
+
+// le cambia el nombre para escribir uint :P
+using uint = unsigned int;
+
+int main()
+{
+	
+	return 69;
+}
