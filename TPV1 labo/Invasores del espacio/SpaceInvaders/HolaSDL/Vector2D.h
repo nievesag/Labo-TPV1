d4d7c8b0dcd45da8712af1c5c1234aaf55d17e68
Inés <ineprimo@ucm.es>
2023-10-18 21:18:05 +0200

    Vector2D piola
    he añadido constructora, operadores y getters ou yea (destructora creo que no hace falta ????)

--- TPV1 labo/Invasores del espacio/SpaceInvaders/HolaSDL/Vector2D.h
+++ TPV1 labo/Invasores del espacio/SpaceInvaders/HolaSDL/Vector2D.h
@@ -15,18 +15,106 @@ private:
 	// metodos publicos
 public:
 	// METODOS
-	// constructora
-	// destructora
-	// getter x
-	// getter y
+	
+	// ---- constructora ----
+	// constructora con las variables declaradas
+	Vector2D::Vector2D(T x, T y)
+		: x(x), y(y) { }
 
-	// OPERADORES
+	// constructora con un vector (deberiamos ponerlo con el setter????)
+	// no entiendo por que da error oop
+	//Vector2D::Vector2D(const Vector2D& vec) 
+	//	: x(vec.getX()), y(vec.getY()) { }
+
+	// ---- destructora ----
+	// lo he pensado otra vez e igual no hace falta una destructora porque no usa memoria
+	// dinamica (aka new) :v (me hacia ilusion el :v)
+	Vector2D::~Vector2D() {
+
+		// ????
+		//delete[] ????;
+	}
+
+	// ---- getters ----
+	// getter x
+	T getX() {
+		return x;
+	}
+	
+	// getter y
+	T getY() {
+		return y;
+	}
+
+	// ---- OPERADORES ----
+	
 	// suma / resta (+/-)
+	// operador +
+	Vector2D<T>& operator+(Vector2D<T> const& other) {
+		Vector2D<T> aux (0,0);
+
+		aux.x = this->getX() + other.x;
+		aux.y = this->getY() + other.y;
+
+		return aux;
+	}
+
+	// operador -
+	Vector2D<T>& operator-(Vector2D<T> const& other) {
+		Vector2D<T> aux(0, 0);
+
+		aux.x = this->getX() - other.x;
+		aux.y = this->getY() - other.y;
+
+		return aux;
+	}
+
 	// producto escalar (*)
+	T& operator*(Vector2D<T> const& other) {
+		// pe = (v1*u2)+(v2+u2)
+		T esc = (getX() * other.x) + (getY() * other.y);
+		return esc;
+	}
+
 	// producto vector-escalar (*)	
+	Vector2D<T>& operator*(int const& other) {
+		Vector2D<T> aux(0, 0);
+
+		aux.x = getX() * other;
+		aux.y = getY() * other;
+
+		return aux;
+	}
+	
+
+	// establecer alias
+	template <typename T> using Point2D = Vector2D;
 };
 
-// establecer alias
-template <typename T> using Point2D = Vector2D;
+
+// CODIGO PARA PROBAR VECTOR2D (PONER EN EL MAIN)
+/*
+	// --- MIRAR QUE SE CREA BIEN
+	Vector2D<int> uwu(1, 2);
+
+	cout << uwu.getX() << " " << uwu.getY() << endl;
+
+	Vector2D<int> owo(2, 1);
+
+	cout << owo.getX() << " " << owo.getY() << endl;
+	
+	// --- MIRAR QUE RESTE/SUME BIEN
+	Vector2D<int> ewe = owo - uwu;
+
+	cout << ewe.getX() << " " << ewe.getY() << endl;
+
+	// --- MIRAR QUE HAGA EL PRODUCTO ESCALAR BIEN
+	cout << (uwu*owo) << endl;
+
+	// --- MIRAR QUE HAGA EL PRODUCTO CON UN ESCALAR BIEN
+	Vector2D<int> iwi = uwu * 2;
+
+	cout << iwi.getX() << " " << iwi.getY() << endl;
+*/
 #endif
 
--- TPV1 labo/Invasores del espacio/SpaceInvaders/HolaSDL/main.cpp
+++ TPV1 labo/Invasores del espacio/SpaceInvaders/HolaSDL/main.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <Windows.h>
 #include <vector>
+#include "Vector2D.h"
 
 using namespace std;
 
@@ -18,5 +19,24 @@ void xd()
 int main(int argc, char* argv[])
 {
 	
+	Vector2D<int> uwu(1, 2);
+
+	cout << uwu.getX() << " " << uwu.getY() << endl;
+	
+	Vector2D<int> owo(2, 1);
+
+	cout << owo.getX() << " " << owo.getY() << endl;
+
+	Vector2D<int> ewe = owo - uwu;
+
+	cout << ewe.getX() << " " << ewe.getY() << endl;
+
+	cout << (uwu*owo) << endl;
+
+	Vector2D<int> iwi = uwu * 2;
+
+	cout << iwi.getX() << " " << iwi.getY() << endl;
+
+
 	return 69;
 }
