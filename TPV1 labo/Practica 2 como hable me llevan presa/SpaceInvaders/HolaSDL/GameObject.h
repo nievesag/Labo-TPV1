d20089b59e753d2d036dee7ed14675d034b26599
Eddochan <ineprimo@ucm.es>
2023-11-21 12:08:08 +0100

    HERENCIAAAAAAAAAAAAAAA

--- TPV1 labo/Practica 2 como hable me llevan presa/SpaceInvaders/HolaSDL/GameObject.h
+++ TPV1 labo/Practica 2 como hable me llevan presa/SpaceInvaders/HolaSDL/GameObject.h
@@ -11,13 +11,36 @@ class Game;
 using namespace std;
 using uint = unsigned int;
 
-class GameObject
+class GameObject : public SceneObject
 {
 	// atributos privados
 private:
 
+	// puntero al game
+	Game* game;
+
 	// metodos publicos
 public:
+
+	GameObject::GameObject(Game* game, Point2D<double> position, int width, int height, int vidas)
+		: game(game), SceneObject(position, width, height, vidas)
+	{
+		// ??????????????????
+	}
+
+	//GameObject::~GameObject()
+
+	// render
+	void const render();
+
+	// update
+	void update();
+
+	// save (guarda partida en un txt)
+	void const save(ostream&);
+
+	// destructora virtual para por polimorfismo
+	virtual GameObject::~GameObject();
 
 };
 #endif
--- TPV1 labo/Practica 2 como hable me llevan presa/SpaceInvaders/HolaSDL/SceneObject.h
+++ TPV1 labo/Practica 2 como hable me llevan presa/SpaceInvaders/HolaSDL/SceneObject.h
@@ -1,17 +1,42 @@
-#ifndef MOTHERSHIP_H
-#define MOTHERSHIP_H
+#ifndef SCENEOBJECT_H
+#define SCENEOBJECT_H
+
+#include "GameObject.h"
+#include "Vector2D.h"
 
 using namespace std;
 using uint = unsigned int;
 
-class SceneObject
+class SceneObject 
 {
 	// atributos privados
 private:
 
+	// posicion del objeto
+	Point2D<double> position;
+
+	// dimension del objeto (height & width)
+	//Vector2D<double> dimension;
+	int width, height;
+
+	// vidas del objeto
+	int vidas;
+
 
 	// metodos publicos
 public:
+
+	SceneObject::SceneObject(Point2D<double> position, int width, int height, int vidas) 
+		: position(position), width(width), height(height), vidas(vidas)
+	{
+		// ????
+	}
+
+	// ataque al objeto (basicamente colisiones)
+	virtual void hit(SDL_Rect ataque, bool frenemy);
+
+	// iterador de objetos (que cojones es esto)
+	//iterator lisObjects;
 
 };
 
