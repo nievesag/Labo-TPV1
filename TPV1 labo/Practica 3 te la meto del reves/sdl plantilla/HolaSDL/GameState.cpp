b98ac9e2654564aaec6fca32f2fd70439e60f396
anonymous <nievalon@ucm.es>
2023-12-12 15:37:59 +0100

    .....

--- TPV1 labo/Practica 3 te la meto del reves/sdl plantilla/HolaSDL/GameState.cpp
+++ TPV1 labo/Practica 3 te la meto del reves/sdl plantilla/HolaSDL/GameState.cpp
@@ -1,2 +1,41 @@
 #include "GameState.h"
 
+void GameState::render()
+{
+}
+
+void GameState::update()
+{
+}
+
+void GameState::save(ostream& file)
+{
+}
+
+bool GameState::onEnter()
+{
+	return false;
+}
+
+bool GameState::onExit()
+{
+	return false;
+}
+
+void GameState::handleEvent(const SDL_Event& event)
+{
+}
+
+void GameState::hasDied(GameList<GameObject, true>::anchor)
+{
+}
+
+void GameState::addEventListener(SDL_Event& event)
+{
+}
+
+void GameState::addObject(SceneObject& object)
+{
+}
+
+
