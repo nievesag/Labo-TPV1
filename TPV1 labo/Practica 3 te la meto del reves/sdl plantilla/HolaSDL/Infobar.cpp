b98981548de4f49bc0a6c4f6e22a320fa2928171
Inés <ineprimo@ucm.es>
2023-12-17 13:48:28 +0100

    base infobar...

--- TPV1 labo/Practica 3 te la meto del reves/sdl plantilla/HolaSDL/Infobar.cpp
+++ TPV1 labo/Practica 3 te la meto del reves/sdl plantilla/HolaSDL/Infobar.cpp
@@ -1,1 +1,25 @@
 #include "Infobar.h"
+
+Infobar::Infobar(const Point2D<>& position, Texture* texture, int padding, PlayState* playState) 
+	: position(position), padding(padding), GameObject(playState)
+{
+
+
+}
+
+void Infobar::render() const
+{
+	// render del coso
+
+}
+
+void Infobar::update()
+{
+	// update del score
+}
+
+void Infobar::save(std::ostream& os) const 
+{
+	// save del coso (esto esta en el playstate actualmente es muy facil de implementar)
+
+}
--- TPV1 labo/Practica 3 te la meto del reves/sdl plantilla/HolaSDL/Infobar.h
+++ TPV1 labo/Practica 3 te la meto del reves/sdl plantilla/HolaSDL/Infobar.h
@@ -1,5 +1,48 @@
+#ifndef INFOBAR_H
+#define INFOBAR_H
+
 #pragma once
-class Infobar
+
+// INCLUDES
+#include "GameObject.h"
+#include <SDL_ttf.h>
+
+
+class Game;
+
+class Infobar : public GameObject
 {
+private:
+
+	// color del score y fuente del score 
+	//TTF_Font* font = TTF_OpenFont(FONT_ROOT.c_str(), 24);
+	SDL_Color color = { 255, 255, 255, 255 };
+
+	// puntuacion
+	int SCORE = 0;
+
+	// padding (separacion con margenes ?)
+	int padding;
+
+	// posicion del score
+	Point2D<double> position;
+
+
+public:
+	// constructora del infobar
+	Infobar(const Point2D<>& position, Texture* texture, int padding, PlayState* playState);
+
+	// render del infobar
+	void render() const override;
+
+	// update del infobar
+	void update() override;
+
+	// save del infobar
+	void save(std::ostream& os) const override {}
+
+
 };
+
+#endif
 
