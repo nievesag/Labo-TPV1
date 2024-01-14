9acb099314011168545ed26bc235db8e9298acba
nievesag <nievalon@ucm.es>
2024-01-14 18:16:14 +0100

    clase font creada

--- TPV1 labo/VERSION DEFINITIVA FULL HD GOLD EDITION/sdl plantilla/HolaSDL/Font.h
+++ TPV1 labo/VERSION DEFINITIVA FULL HD GOLD EDITION/sdl plantilla/HolaSDL/Font.h
@@ -5,14 +5,17 @@
 
 #include <iostream>
 #include <SDL.h>
-#include <SDL_ttf.h>
 #include "texture.h"
+
+// biblioteca ttf
+#include <SDL_ttf.h>
 
 // ENUNCIADO:
 // Utiliza el paquete TTF de SDL para manejar los distintos textos del juego
 // Encapsular la interacción con la biblioteca en la clase Font
 // definir un objeto del juego Label para colocar piezas de texto en la pantalla
-// (Button podría ser subtipo de Label).
+// (Button podría ser subtipo de Label)
+// basado en las diapositivas del tema 7
 class Font
 {
 	// atributos privados
@@ -25,13 +28,16 @@ public:
 	Font(const Font& font) = delete;
 	~Font();
 
+	// crea una fuente a partir de un archivo
 	void load(const std::string& filename, int size);
+
+	// para liberar una font creada previamente
 	void Free();
 
-
+	// genera una surface
 	SDL_Surface* generateSurface(const std::string& text, SDL_Color color) const;
 
-
+	// generacion de textura a partir de una fuente y un texto
 	Texture* generateTexture(SDL_Renderer* renderer, const std::string& text, SDL_Color color) const;
 };
 
